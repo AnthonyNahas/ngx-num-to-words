7908109d3101bd9cc81ff8451af5cc19a51616a5
Anthony Nahas <anthony.na@hotmail.de>
2020-11-30 23:07:56 +0100

    fix(demo): enhanced and improved the demo app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { MatSelectChange } from '@angular/material/select';
+import { UntilDestroy, untilDestroyed } from '@ngneat/until-destroy';
+import { NgxNumToWordsService, SUPPORTED_LANGUAGE } from 'ngx-num-to-words';
+import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
 
+@UntilDestroy()
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'ngx-num-to-word-demo';
+  fc = new FormControl(123);
+  numberInWords!: string;
+  lang!: SUPPORTED_LANGUAGE;
+  initialValue = 123;
+
+  constructor(private ngxNumToWordsService: NgxNumToWordsService) {
+  }
+
+  selectionChange($event: MatSelectChange): void {
+    console.log('selectionChange', $event);
+    this.lang = $event.value;
+    this.numberInWords = this.ngxNumToWordsService.inWords(+this.fc.value, this.lang);
+  }
+
+  ngOnInit(): void {
+
+    this.numberInWords = this.ngxNumToWordsService.inWords(+this.fc.value);
+
+    this.fc
+      .valueChanges
+      .pipe(untilDestroyed(this), debounceTime(400), distinctUntilChanged())
+      .subscribe((value => {
+        console.log('value ->', value);
+        this.numberInWords = this.ngxNumToWordsService.inWords(+value, this.lang);
+      }));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,13 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexModule } from '@angular/flex-layout';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MarkdownModule } from 'ngx-markdown';
+import { NgxNumToWordsModule } from 'ngx-num-to-words';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +19,15 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MarkdownModule.forRoot(),
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
+    FlexModule,
+    NgxNumToWordsModule,
+    MatInputModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
