84a8e6dd73578ced602778453d20b7872f42b2cf
Anthony Nahas <>
2023-11-05 10:09:29 +0100

    fix(project): MINOR

--- projects/ngx-num-to-words/src/lib/in-words.pipe.ts
+++ projects/ngx-num-to-words/src/lib/in-words.pipe.ts
@@ -1,7 +1,7 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 // @ts-ignore
 import n2words from 'n2words';
-import { SUPPORTED_LAUAGE } from './i18n';
+import {SUPPORTED_LANGUAGE} from './i18n';
 
 @Pipe({
   name: 'inWords'
@@ -9,7 +9,7 @@ import { SUPPORTED_LAUAGE } from './i18n';
 export class InWordsPipe implements PipeTransform {
 
   transform(value: number, lang?: SUPPORTED_LANGUAGE): unknown {
-    return n2words(value, { lang });
+    return n2words(value, {lang});
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    initialNavigation: 'enabled'
-})],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
